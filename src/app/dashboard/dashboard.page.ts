3aea35420b0be90200b51cf72fb2279cf24d293e
Sam Grumley <grumsamu@gmail.com>
2020-07-15 06:46:02 -0700

    initial commit

---
+++ src/app/dashboard/dashboard.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import {AuthenticationService} from '../services/authentication.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.scss'],
+})
+
+export class DashboardPage implements OnInit {
+  userEmail: string;
+
+  constructor(
+    private navCtrl: NavController,
+    private authService: AuthenticationService
+  ) { }
+
+  ngOnInit() {
+    this.authService.userDetails().subscribe(res => {
+      console.log('res ', res);
+      if (res !== null){
+        this.userEmail = res.email;
+      } else {
+        this.navCtrl.navigateBack('');
+      }
+    }, err => {
+      console.log('err', err);
+    })
+  }
+
+  logout() {
+    this.authService.logoutUser()
+    .then(res => {
+      console.log(res);
+      this.navCtrl.navigateBack('');
+    })
+    .catch( error => {
+      console.log(error);
+    })
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCR1tXABEbyc2kpc-0E95TW2Y52Iu9QtNs",
+    authDomain: "dinesafe-5f15a.firebaseapp.com",
+    databaseURL: "https://dinesafe-5f15a.firebaseio.com",
+    projectId: "dinesafe-5f15a",
+    storageBucket: "dinesafe-5f15a.appspot.com",
+    messagingSenderId: "641810368179",
+    appId: "1:641810368179:web:3743a91c06c62abc91d2c6",
+    measurementId: "G-NZ096R000V"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
